d3f217f10fc1d6a9f6484b86a015c83a7d505adf
Kevin Duff <kevinkelduff@gmail.com>
2022-02-08 23:04:12 +0000

    Remove players

--- src/gateau_api/routers/web_app.py
+++ src/gateau_api/routers/web_app.py
@@ -5,7 +5,7 @@ Route intended to interface with the Web App
 import logging
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi_camelcase import CamelModel
 
 from gateau_api.dependencies import get_service, get_user_uid
@@ -19,12 +19,27 @@ router = APIRouter(dependencies=[Depends(get_user_uid)])
 
 
 @router.post("/game/{gameId}/players")
-async def post_player(
+async def join_game(
     gameId: str,
     player: Player,
     service: GateauFirebaseService = Depends(get_service),
+    player_id: str = Depends(get_user_uid),
 ):
+    if player.uid != player_id:
+        raise HTTPException(
+            status_code=401,
+            detail="Cannot add someone else to the game",
+        )
     service.set_player(game_id=gameId, player=player)
+
+
+@router.delete("/game/{gameId}/players")
+async def leave_game(
+    gameId: str,
+    service: GateauFirebaseService = Depends(get_service),
+    player_id: str = Depends(get_user_uid),
+):
+    service.remove_player(game_id=gameId, player_id=player_id)
 
 
 class NewSubscriptions(CamelModel):
