3eb909a70ca9b64ebf54dd4592fbe74a190efb9c
Kevin Duff <kevinkelduff@gmail.com>
2022-02-08 20:14:02 +0000

    Fix auth

--- src/gateau_api/common/get_service.py
+++
@@ -1,5 +0,0 @@
-from gateau_api.service import GateauFirebaseService
-
-
-async def get_service() -> GateauFirebaseService:
-    return GateauFirebaseService()
--- src/gateau_api/firebase.py
+++ src/gateau_api/firebase.py
@@ -1,16 +1,35 @@
 import base64
 import json
+from typing import Any, Dict
 
 import firebase_admin
+from pyrebase import initialize_app
 
-from gateau_api.constants import FIREBASE_ADMIN_CREDENTIALS
+from gateau_api.constants import (
+    FIREBASE_ADMIN_CREDENTIALS,
+    FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DATABASE_URL,
+    FIREBASE_STORAGE_BUCKET,
+)
+
+
+def service_account_info() -> Dict[str, Any]:
+    return json.loads(base64.b64decode(FIREBASE_ADMIN_CREDENTIALS).decode("utf-8"))
 
 
 def firebase_init_app():
-    service_account_info = json.loads(
-        base64.b64decode(FIREBASE_ADMIN_CREDENTIALS).decode("utf-8")
-    )
-    creds = firebase_admin.credentials.Certificate(service_account_info)
+    creds = firebase_admin.credentials.Certificate(service_account_info())
 
     if not firebase_admin._apps:
         firebase_admin.initialize_app(creds)
+
+
+def pyrebase_app():
+    config = {
+        "databaseURL": FIREBASE_DATABASE_URL,
+        "apiKey": FIREBASE_API_KEY,
+        "authDomain": FIREBASE_AUTH_DOMAIN,
+        "storageBucket": FIREBASE_STORAGE_BUCKET,
+    }
+    return initialize_app(config)
